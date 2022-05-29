049f1d7c57a2afba92dbdb4c7293656740c59aeb
mikseros <mmadynski@gmail.com>
2022-05-29 22:58:36 +0200

    CRUD for Cart Entity (Service)

--- src/main/java/com/mikseros/onetomany/service/CartService.java
+++ src/main/java/com/mikseros/onetomany/service/CartService.java
@@ -1,5 +1,51 @@
 package com.mikseros.onetomany.service;
 
-public class CartService {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mikseros.onetomany.exception.CartNotFoundException;
+import com.mikseros.onetomany.model.Cart;
+import com.mikseros.onetomany.repository.CartRepository;
+
+@Service
+public class CartService {
+	
+	private final CartRepository cartRepository;
+	private final ItemService itemService;
+	
+	@Autowired
+	public CartService(CartRepository cartRepository, ItemService itemService) {
+		this.cartRepository = cartRepository;
+		this.itemService = itemService;
+	}
+	
+	public Cart addCart(Cart cart) {
+		return cartRepository.save(cart);
+	}
+	
+	public List<Cart> getCarts() {
+		return StreamSupport.stream(cartRepository.findAll().spliterator(), false)
+				.collect(Collectors.toList());
+	}
+	
+	public Cart getCart(Long id) {
+		return cartRepository.findById(id).orElseThrow(() -> 
+				new CartNotFoundException(id));
+	}
+	
+	public Cart deleteCart(Long id) {
+		Cart cart = getCart(id);
+		cartRepository.delete(cart);
+		return cart;
+	}
+	
+	public Cart editCart(Long id, Cart cart) {
+		Cart cartToEdit = getCart(id);
+		cartToEdit.setName(cart.getName());
+		return cartToEdit;
+	}
 }
