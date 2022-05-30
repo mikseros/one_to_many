31b9f155f6da4e79f17c29b779490fecd7d1f579
mikseros <mmadynski@gmail.com>
2022-05-30 12:55:26 +0200

    Cart controller(PostMapping - addCart function)

--- src/main/java/com/mikseros/onetomany/controller/CartController.java
+++ src/main/java/com/mikseros/onetomany/controller/CartController.java
@@ -1,5 +1,31 @@
 package com.mikseros.onetomany.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mikseros.onetomany.model.Cart;
+import com.mikseros.onetomany.model.dto.CartDto;
+import com.mikseros.onetomany.service.CartService;
+
+@RestController
+@RequestMapping("/carts")
 public class CartController {
 
+	private final CartService cartService;
+	
+	@Autowired
+	public CartController(CartService cartService) {
+		this.cartService = cartService;
+	}
+	
+	@PostMapping
+	public ResponseEntity<CartDto> addCart(@RequestBody final CartDto cartDto) {
+		Cart cart = cartService.addCart(Cart.from(cartDto));
+		return new ResponseEntity<>(CartDto.from(cart), HttpStatus.OK);
+	}
 }
