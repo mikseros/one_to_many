b557d54505210c88776e69048e0a671cd8754e2c
mikseros <mmadynski@gmail.com>
2022-05-30 12:40:04 +0200

    get, delete, edit item (by id)

--- src/main/java/com/mikseros/onetomany/controller/ItemController.java
+++ src/main/java/com/mikseros/onetomany/controller/ItemController.java
@@ -6,8 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +41,24 @@ public class ItemController {
 		List<Item> items = itemService.getItems();
 		List<ItemDto> itemsDto = items.stream().map(ItemDto::from).collect(Collectors.toList());
 		return new ResponseEntity<>(itemsDto, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "{id}")
+	public ResponseEntity<ItemDto> getItem(@PathVariable final Long id) {
+		Item item = itemService.getItem(id);
+		return new ResponseEntity<>(ItemDto.from(item), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "{id}")
+	public ResponseEntity<ItemDto> deleteItem(@PathVariable final Long id) {
+		Item item = itemService.deleteItem(id);
+		return new ResponseEntity<>(ItemDto.from(item), HttpStatus.OK);
+	}
+	
+	@PutMapping(value = "{id}")
+	public ResponseEntity<ItemDto> editItem(@PathVariable final Long id,
+											@RequestBody final ItemDto itemDto) {
+		Item editedItem = itemService.editItem(id, Item.from(itemDto));
+		return new ResponseEntity<>(ItemDto.from(editedItem), HttpStatus.OK);
 	}
 }
