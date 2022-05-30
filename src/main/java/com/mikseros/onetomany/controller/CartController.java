48a40d242e9e3a8b1acd522bd36a1bd0ce93ba26
mikseros <mmadynski@gmail.com>
2022-05-30 13:01:01 +0200

    CartController: Get All Carts (@GetMapping)

--- src/main/java/com/mikseros/onetomany/controller/CartController.java
+++ src/main/java/com/mikseros/onetomany/controller/CartController.java
@@ -1,8 +1,12 @@
 package com.mikseros.onetomany.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +31,12 @@ public class CartController {
 	public ResponseEntity<CartDto> addCart(@RequestBody final CartDto cartDto) {
 		Cart cart = cartService.addCart(Cart.from(cartDto));
 		return new ResponseEntity<>(CartDto.from(cart), HttpStatus.OK);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CartDto>> getCarts() {
+		List<Cart> carts = cartService.getCarts();
+		List<CartDto> cartsDto = carts.stream().map(CartDto::from).collect(Collectors.toList());
+		return new ResponseEntity<>(cartsDto, HttpStatus.OK);
 	}
 }
